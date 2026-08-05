d4a153c9716733fa42fcbf19bc218ab93d57257a
lenyaplay <lenyaplay@mail.ru>
2026-08-05 14:13:30 +0300

    feat: add brackets validator

--- src/test/kotlin/BracketTestCase.kt
+++
@@ -1,9 +0,0 @@
-import kotlinx.serialization.Serializable
-
-@Serializable
-data class BracketTestCase (
-    val id : Int,
-    val input: String,
-    val expected: Boolean,
-    val comment: String
-)
