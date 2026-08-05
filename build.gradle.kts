736db6bbd932ca209c78a713261c275b19da3730
lenyaplay <lenyaplay@mail.ru>
2026-08-05 14:25:58 +0300

    feat: add brackets validator

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,7 @@
 plugins {
     kotlin("jvm") version "2.4.0"
+    kotlin("plugin.serialization") version "2.3.20"
+
 }
 
 group = "org.example"
@@ -11,6 +13,7 @@ repositories {
 
 dependencies {
     testImplementation(kotlin("test"))
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.11.0")
 }
 
 kotlin {
