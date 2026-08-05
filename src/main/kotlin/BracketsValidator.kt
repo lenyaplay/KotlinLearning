a58cd83d8594fc82f798bb2ebbb27bf0d7fde26d
lenyaplay <lenyaplay@mail.ru>
2026-08-05 13:56:11 +0300

    feat: add manual docs, add new test case, fix alg

--- src/main/kotlin/BracketsValidator.kt
+++ src/main/kotlin/BracketsValidator.kt
@@ -1,31 +1,31 @@
 package org.example
 
-import java.util.Stack
-
 class BracketsValidator {
     companion object {
+        private val pairs = mapOf('(' to ')', '[' to ']', '{' to '}')
+        private val closers = pairs.values.toSet()
+
         fun isValid(str: String): Boolean {
-            val pairs = mapOf(
-                '(' to ')',
-                '[' to ']',
-                '{' to '}'
-            )
-            val stack = Stack<Char>()
+            val deque = ArrayDeque<Char>()
+            // открыватель - (, [, {
+            // закрыватель - ), ], }
             for (char in str) {
-                val opener = pairs.containsKey(char)
-                if(!pairs.containsKey(char) && !pairs.containsValue(char)) return false
-                if(opener) {
-                    val closer = pairs[char]
-                    stack.push(closer)
-                    continue
-                }
-                if(stack.isEmpty()) return false
-                if(char == stack.lastOrNull()) {
-                    stack.pop()
-                    continue
+                // допустим наш символ это открыватель, то надо выяснить его закрыватель
+                // если вместо стандартного символа пришел другой, мы получим null, так как для него нет закрывателя
+                val closer = pairs[char]
+                when {
+                    // char оказался открывателем, потому добавляем его закрыватель в стек
+                    closer != null -> deque.addLast(closer)
+                    // char оказался закрывателем - проверяем что в стеке последний элемент именно он
+                    char in closers -> {
+                        if (deque.isEmpty() || deque.removeLast() != char) return false
+                    }
+                    // наш символ не открыватель и не закрыватель, значит строка не валидная
+                    else -> return false
                 }
             }
-            return stack.isEmpty()
+            // проверяем что каждый открыватель закрылся и, следовательно, строка валидная
+            return deque.isEmpty()
         }
     }
 }
