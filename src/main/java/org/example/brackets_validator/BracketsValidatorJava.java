adfe3d25a1a7043d5622c9b0a962c03019a7f1cf
lenyaplay <lenyaplay@mail.ru>
2026-08-06 06:55:51 +0300

    feat: add java implementation

--- src/main/java/org/example/brackets_validator/BracketsValidatorJava.java
+++ src/main/java/org/example/brackets_validator/BracketsValidatorJava.java
@@ -1,8 +1,13 @@
 package org.example.brackets_validator;
 
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+import java.util.Stack;
+
 public final class BracketsValidatorJava {
-    private BracketsValidatorJava() {
-    }
+    static Map<Character, Character> pairs = Map.of('(', ')', '[', ']', '{', '}');
+    static Set<Character> closers = new HashSet<>(pairs.values());
 
     /**
      * Проверяет, является ли строка корректной скобочной последовательностью.
@@ -12,7 +17,18 @@ public final class BracketsValidatorJava {
      * @return {@code true}, если скобочная последовательность корректна, иначе {@code false}
      */
     public static boolean isValid(String str) {
-        // TODO: реализовать
-        return false;
+        var stack = new Stack<Character>();
+        for (var ch : str.toCharArray()) {
+            var closer = pairs.get(ch);
+            if (closer != null) {
+                stack.push(closer);
+            } else if (closers.contains(ch)) {
+                if (stack.isEmpty() || stack.removeLast() != ch) return false;
+            } else {
+                return false;
+            }
+
+        }
+        return stack.isEmpty();
     }
 }
